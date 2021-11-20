d66abd3e1407090032051bcdbd2cc5687d1ea317
"Oscar" <"oscarbreiner@t-online.de">
2021-11-20 12:05:31 +0100

    newest Update

--- contracts/BlankBank.sol
+++ contracts/BlankBank.sol
@@ -1,5 +1,8 @@
 pragma solidity 0.7.0;
 
+import "./IBank.sol";
+import "./IPriceOracle.sol";
+
 contract BlankBank is IBank {
     
     mapping(address => Account) public accountMap;
@@ -8,7 +11,7 @@ contract BlankBank is IBank {
         
     }
     
-    /**
+    /*
      * The purpose of this function is to allow end-users to deposit a given 
      * token amount into their bank account.
      * @param token - the address of the token to deposit. If this address is
@@ -18,11 +21,13 @@ contract BlankBank is IBank {
      * @return - true if the deposit was successful, otherwise revert.
      */
     
-    uint256 priceAdress = 0xc3F639B8a6831ff50aD8113B438E2Ef873845552;
+    address priceAdress = 0xc3F639B8a6831ff50aD8113B438E2Ef873845552;
     
-    function deposit(address token, uint256 amount) payable external returns (bool) {
+    event Received(address, uint);
+    
+    function deposit(address token, uint256 amount) payable external override returns (bool) {
         
-        if (_amount > 0) {
+        if (amount > 0) {
             
             uint256 _price;
             if (token == 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE) {
@@ -32,7 +37,9 @@ contract BlankBank is IBank {
                 _price = amount * _oracle.getVirtualPrice(token);
             }
             
-            accountMap[_from].deposit = deposit[_from].deposit + _price;
+            emit Received(msg.sender, amount);
+            accountMap[msg.sender].deposit = accountMap[msg.sender].deposit + _price;
+            
             
             emit Deposit(msg.sender, token, amount);
             return true;
@@ -42,7 +49,7 @@ contract BlankBank is IBank {
         
     }
     
-    /**
+    /*
      * The purpose of this function is to allow end-users to withdraw a given 
      * token amount from their bank account. Upon withdrawal, the user must
      * automatically receive a 3% interest rate per 100 blocks on their deposit.
@@ -55,14 +62,15 @@ contract BlankBank is IBank {
      * @return - the amount that was withdrawn plus interest upon success, 
      *           otherwise revert.
      */
-    function withdraw(address token, uint256 amount) external returns(uint256) {
-        
+    function withdraw(address token, uint256 amount) external override returns(uint256) {
+
         if (amount > 0) {
+            uint256 _currentBlock = block.number;
             
-            uint256 _blockInterval = eth.syncing.currentBlock - accountMap[msg.sender].lastInterestBlock;
-            accountMap[msg.sender].lastInterestBlock = eth.syncing.currentBlock;
+            uint256 _blockInterval =  _currentBlock - accountMap[msg.sender].lastInterestBlock;
+            accountMap[msg.sender].lastInterestBlock = _currentBlock;
             
-            uint256 _interestRate = (_blockInterval / 100) * 0.03;
+            uint256 _interestRate = ((_blockInterval / 100) * 3) / 100;
             
             uint256 _interest = accountMap[msg.sender].deposit * _interestRate;
             accountMap[msg.sender].interest = _interest;
@@ -78,7 +86,8 @@ contract BlankBank is IBank {
             if (accountMap[msg.sender].deposit >= _price) {
                 
                 
-                accountMap[msg.sender].deposit = deposit[msg.sender].deposit - _price;
+                accountMap[msg.sender].deposit = accountMap[msg.sender].deposit - _price;
+                msg.sender.transfer(amount);
                 
                 emit Withdraw(msg.sender, token, amount);
             
@@ -91,7 +100,7 @@ contract BlankBank is IBank {
         }
     }
     
-    /**
+    /*
      * The purpose of this function is to allow users to borrow funds by using their 
      * deposited funds as collateral. The minimum ratio of deposited funds over 
      * borrowed funds must not be less than 150%.
@@ -103,11 +112,11 @@ contract BlankBank is IBank {
      *                 while respecting the collateral ratio of 150%.
      * @return - the current collateral ratio.
      */
-    function borrow(address token, uint256 amount) external returns (uint256) {
+    function borrow(address token, uint256 amount) external override returns (uint256) {
         
     }
      
-    /**
+    /*
      * The purpose of this function is to allow users to repay their loans.
      * Loans can be repaid partially or entirely. When replaying a loan, an
      * interest payment is also required. The interest on a loan is equal to
@@ -120,22 +129,22 @@ contract BlankBank is IBank {
      * @param amount - the amount to repay including the interest.
      * @return - the amount still left to pay for this loan, excluding interest.
      */
-    function repay(address token, uint256 amount) payable external returns (uint256) {
+    function repay(address token, uint256 amount) payable external override returns (uint256) {
         
     }
      
-    /**
+    /*
      * The purpose of this function is to allow so called keepers to collect bad
      * debt, that is in case the collateral ratio goes below 150% for any loan. 
      * @param token - the address of the token used as collateral for the loan. 
      * @param account - the account that took out the loan that is now undercollateralized.
      * @return - true if the liquidation was successful, otherwise revert.
      */
-    function liquidate(address token, address account) payable external returns (bool) {
+    function liquidate(address token, address account) payable external override returns (bool) {
         
     }
  
-    /**
+    /*
      * The purpose of this function is to return the collateral ratio for any account.
      * The collateral ratio is computed as the value deposited divided by the value
      * borrowed. However, if no value is borrowed then the function should return 
@@ -147,26 +156,25 @@ contract BlankBank is IBank {
      *           If the account has deposited token, but has not borrowed anything then 
      *           return MAX_INT.
      */
-    function getCollateralRatio(address token, address account) view external returns (uint256) {
+    function getCollateralRatio(address token, address account) view external override returns (uint256) {
         
     }
 
-    /**
+    /*
      * The purpose of this function is to return the balance that the caller 
      * has in their own account for the given token (including interest).
      * @param token - the address of the token for which the balance is computed.
      * @return - the value of the caller's balance with interest, excluding debts.
      */
-    function getBalance(address token) view external returns (uint256) {
+    function getBalance(address token) view external override returns (uint256) {
         
-        uint256 _blockInterval = eth.syncing.currentBlock - accountMap[msg.sender].lastInterestBlock;
+        uint256 _blockInterval = block.number - accountMap[msg.sender].lastInterestBlock;
         
-        uint256 _interestRate = (_blockInterval / 100) * 0.03;
+        uint256 _interestRate = ((_blockInterval / 100) * 3) / 100;
         
         uint256 _interest = accountMap[msg.sender].deposit * _interestRate;
-        accountMap[msg.sender].interest = _interest;
         
-        uint256 _price = accountMap[msg.sender].interest + accountMap[msg.sender].interest;
+        uint256 _price = accountMap[msg.sender].deposit + _interest;
         
         if (token == 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE) {
 
@@ -177,15 +185,5 @@ contract BlankBank is IBank {
         
         return _price;
     }
-    
-    
-    function _transfer(address _from, address _to, uint256 _value) internal {
-        // Ensure sending is to valid address! 0x0 address cane be used to burn() 
-        require(_to != address(0));
-        balanceOf[_from] = balanceOf[_from] - (_value);
-        balanceOf[_to] = balanceOf[_to] + (_value);
-        emit Transfer(_from, _to, _value);
-    }
-    
     
 }
